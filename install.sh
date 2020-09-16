7d1eeffae6e9226bcef1bd75bceee7dddce69bde
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-16 15:09:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,13 +7,14 @@ sed -i "s| main| main contrib non-free|g" '/etc/apt/sources.list'
 apt-get -y update \
     && apt-get -y install wget locales
 
-# remove non-UTF-8 locales and enable some locales (enabling all make building very slow)
+# remove non-UTF-8 locales, enable some locales (enabling all make building very slow), set to en_GB for default
 sed -i -e "/UTF-8/!d" /etc/locale.gen \
     && sed -i -e "s/# en_GB/en_GB/g" /etc/locale.gen \
     && sed -i -e "s/# en_US/en_US/g" /etc/locale.gen \
-    && dpkg-reconfigure --frontend=noninteractive locales
+    && dpkg-reconfigure --frontend=noninteractive locales \
+    && update-locale LANG='en_GB.UTF-8'
 
-# install openvpn
+# install openvpn and nft
 apt-get install -y openvpn
 apt-get install -y nftables
 
@@ -32,7 +33,7 @@ apt-get -y install tinyproxy \
     && rm -rf /etc/tinyproxy/*
 
 # remove sabnzbdplus config
-apt-get -y install unzip sabnzbdplus
+apt-get -y install unzip sabnzbdplus \
     && rm -rf /etc/init.d/sabnzbdplus \
     && rm -rf /etc/default/sabnzbdplus
 
