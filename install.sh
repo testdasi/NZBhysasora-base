0fccd206e6079e438e44eec56c23d273682ccb22
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-16 11:12:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# add contrib and non-free repos. sab is in contrib
+sed -i "s| main| main contrib non-free|g" '/etc/apt/sources.list'
+
 # install more packages
 apt-get -y update \
-    && apt-get -y install wget
+    && apt-get -y install wget sabnzbdplus
