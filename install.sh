77880cb81dc39a2462af1c0db9d68cd67f1d9b4e
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-16 11:21:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,13 @@ sed -i "s| main| main contrib non-free|g" '/etc/apt/sources.list'
 # install more packages
 apt-get -y update \
     && apt-get -y install wget sabnzbdplus
+
+# remove sabnzbdplus config
+rm -rf /etc/init.d/sabnzbdplus \
+    && rm -rf /etc/default/sabnzbdplus
+
+# clean up
+#apt-get -y autoremove \
+#    && apt-get -y autoclean \
+#    && apt-get -y clean \
+#    && rm -fr /tmp/* /var/tmp/* /var/lib/apt/lists/*
